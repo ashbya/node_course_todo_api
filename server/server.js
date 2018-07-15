8fe87c3029225b9e29f7fa5e45f45b7738c360f7
ashbya <avery.ashby@gmail.com>
2018-07-15 18:11:28 -0500

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mangoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -25,6 +26,22 @@ app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
   }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => { //Promise fullfilled
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }, (e) => {   // Promise rejected
     res.status(400).send(e);
   });
 });
