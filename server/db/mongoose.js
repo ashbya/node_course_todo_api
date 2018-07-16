d32966bfa60d89313c36c8dad837cb7ab596047a
ashbya <avery.ashby@gmail.com>
2018-07-15 19:25:32 -0500

    Setup app for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;    //versus BlueBird Promise
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGOD_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
