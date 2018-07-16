99d1b1b9645ffd043b0c94cd5310e73562940c64
ashbya <avery.ashby@gmail.com>
2018-07-15 23:33:12 -0500

    Setup separate test and dev env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;    //versus BlueBird Promise
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
