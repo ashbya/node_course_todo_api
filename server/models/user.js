451bb60a30b377d1077e16b3175f597798cfd517
ashbya <avery.ashby@gmail.com>
2018-07-21 11:44:39 -0500

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -69,6 +69,24 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
+// Model method findByCredentials
+UserSchema.statics.findByCredentials = function(email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) { return Promise.reject(); }
+
+    // bcyrpt does not support Promises, roll your own
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (err) { return Promise.reject(); }
+        if (res) { resolve(user); }
+        else     { reject(user);  }
+      });
+    });
+  });
+};
+
 UserSchema.pre('save', function (next) {
     var user = this;
     if(user.isModified('password')) {
